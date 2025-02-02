403269815ded8b6ac2582be034b980f688260d53
NS <diamondintheruffvendorevents@outlook.com>
2025-02-01 21:12:36 -0500

    Source

---
+++ 0x00/0x00.h
@@ -0,0 +1,22 @@
+#pragma once
+#pragma warning( disable: 4152 )
+#pragma warning( disable: 4201 )
+
+#pragma warning( push, 0 )
+#include <windows.h>
+#pragma warning( pop )
+
+#include <accctrl.h>
+#include <aclapi.h>
+#include <stdio.h>
+#include <math.h>
+#include "Math.h"
+#include "Destruction.h"
+#include "System.h"
+#include "Payloads.h"
+#include "Initialize.h"
+
+
+#pragma region Public Variables
+#pragma comment( lib, "advapi32" )
+#pragma endregion Public Variables
